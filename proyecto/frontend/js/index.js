5a28195324cab6a477de51a9f39e9c6b22da1f4e
Alumne DAM <alumne.dam.ieseljust@gmail.com>
2025-05-23 11:50:19 +0200

    index.js

--- proyecto/frontend/js/index.js
+++ proyecto/frontend/js/index.js
@@ -1,3 +1,32 @@
+
+document.addEventListener('DOMContentLoaded',async() =>{
+    try {
+        const tocken = localStorage.getItem('token');
+        if (tocken) {
+            console.log(tocken);
+            const response = await fetch('/tocken', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ "tocken":token })
+            });
+
+            if (!response.status) {
+                throw new Error(response.statusText);
+            }
+
+            const data = await response.json();
+            console.log("Data received:", data);
+        } else {
+            console.warn("No token found in local storage.");
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+})
+
+
 const form = document.getElementById('openproject');
 
 
@@ -5,11 +34,10 @@ form.addEventListener('submit', async (e) => {
     // Inhibim l'enviament automàtic del formulari
     e.preventDefault();
 
-
     
     const formData = new FormData(form);
 
-   
+
     try {
         const response = await fetch('/enviar-matricula', {
             method: 'POST',
